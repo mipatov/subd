0a4db129a5ef93db30e1bd84d4480048303ac727
mipatov <lolpie2410@gmail.com>
2020-09-15 12:56:29 +0300

    главная программа запускает форму с таблицей

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pymysql
 from pymysql.cursors import DictCursor
 from contextlib import closing
+import sys
+from PyQt5 import QtWidgets
 
-with closing(pymysql.connect('localhost', 'root','root', 'nir',cursorclass=DictCursor)) as con:
-    with con.cursor() as cur:
-        pass
-        # query = "SELECT CODPROG, count(*) cnt FROM ntp_proj GROUP by CODPROG order by Codprog"
-    con.commit()
+from TableWindow import *
+
+
+
 
 def CountNtpInProg(con):
     with con.cursor() as cur:
@@ -18,3 +19,35 @@ def CountNtpInProg(con):
             q = "UPDATE ntp_prog SET nproj = {} WHERE TRIM(codprog) = {}".format(row['cnt'], row['CODPROG'])
             cur.execute(q)
         con.commit()
+
+def SumPFinInProg(con):
+    with con.cursor() as cur:
+        cur.execute("select codprog ,sum(PFIN) pfin,sum(PFIN1) pfin1,sum(PFIN2) pfin2,sum(PFIN3) pfin3,sum(PFIN4) pfin4 from nir.ntp_proj Group by ntp_proj.codprog")
+        ntp_prog_pfin_sum = cur.fetchall()
+
+        cur.execute("SET SQL_SAFE_UPDATES = 0")
+        for row in ntp_prog_pfin_sum:
+            q = "UPDATE ntp_prog SET pfin = {},pfin1 = {},pfin2 = {},pfin3 = {},pfin4 = {} WHERE TRIM(codprog) = {}".format(row['pfin'],row['pfin1'],row['pfin2'],row['pfin3'],row['pfin4'], row['codprog'])
+            cur.execute(q)
+        con.commit()
+
+def GetTableNtp(con):
+    with con.cursor() as cur:
+        cur.execute("SELECT pg.PROG, pj.F, pj.SROK_N, pj.SROK_K, pj.isp, pj.RUK, pj.PFIN, pj.FFIN,  pj.GRNTI, pj.CODTYPE, pj.NIR FROM nir.ntp_proj pj, nir.ntp_prog pg where pj.CODPROG = pg.CODPROG;")
+        table = cur.fetchall()
+        return table
+
+def main():
+    with closing(pymysql.connect('localhost', 'root', 'root', 'nir', cursorclass=DictCursor)) as con:
+        ntpTable = GetTableNtp(con)
+
+    # print(ntpTable[0]["F"])
+    app = QtWidgets.QApplication([])
+    window = TableWidgetWindow(ntpTable)
+    window.show()
+    app.exec()
+
+
+
+if __name__ == '__main__':
+    main()
